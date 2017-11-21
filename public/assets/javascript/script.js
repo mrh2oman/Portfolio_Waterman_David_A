34fc721727edcaf45cfb300b625732eac27fbd9e
David Waterman <davidwaterman@Davids-MacBook-Air.local>
2017-11-21 11:22:20 -0600

    name change because people are sensitive

---
+++ public/assets/javascript/script.js
@@ -0,0 +1,12 @@
+  var config = {
+    apiKey: "AIzaSyCsAf9l1KfjuVpm2cdVD6z8xIcjd_4hHtQ",
+    authDomain: "my-new-project-cf87b.firebaseapp.com",
+    databaseURL: "https://my-new-project-cf87b.firebaseio.com",
+    projectId: "my-new-project-cf87b",
+    storageBucket: "my-new-project-cf87b.appspot.com",
+    messagingSenderId: "257872785373"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
